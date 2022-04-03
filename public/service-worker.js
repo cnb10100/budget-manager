7982c85f46e7f5ba4677e984548b533e4e15538b
Cysmi <cnb10100@gmail.com>
2022-04-03 02:38:49 -0400

    finished service worker/activated

--- public/service-worker.js
+++ public/service-worker.js
@@ -46,3 +46,19 @@ const FILES_TO_CACHE = [
     })
   );
 });
+
+  // intercept fetch requests
+  self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+      })
+    )
+  })
